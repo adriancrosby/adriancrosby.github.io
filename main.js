9760051e0ce0716ef914713fd90bb0151cec5fbf
adriancrosby <96785884+adriancrosby@users.noreply.github.com>
2022-01-03 11:35:29 -0800

    Added weather widget

--- main.js
+++ main.js
@@ -28,8 +28,35 @@ const months = {
 }
 
 
+function weatherBallon(cityID) {
+  var key = 'af2b0132ec478c60dfa3990f4334016b'
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)
+  .then(function(resp) {return resp.json()})
+  .then(function(data) {
+    drawWeather(data);
+  })
+  .catch(function() {
+    console.log('error bro')
+  })
+}
+
+function drawWeather(d) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+
+  document.getElementById('description').innerHTML = d.weather[0].description;
+  document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+  document.getElementById('location').innerHTML = d.name;
+}
+
+window.onload = function() {
+  weatherBallon(5381325)
+}
+
+
 const monthNumber = Object.keys(months)
 console.log(parseInt(monthNumber))
+
 
 function monthWord() {
   for (let i = 0; i < 20; i++) {
